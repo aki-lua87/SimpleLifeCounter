d82b341c8ed33fdc3937155bb958c42281e747f5
aki_lua87 <aki_lua87@outlook.jp>
2016-06-09 02:04:04 +0900

    WP:file.nameが読めないのでいったん削除　あとIgnore

--- SimpleLifeCounter/SimpleLifeCounter.WinPhone/SaveAndLoad_WinPhone.cs
+++ SimpleLifeCounter/SimpleLifeCounter.WinPhone/SaveAndLoad_WinPhone.cs
@@ -12,60 +12,8 @@ using SimpleLifeCounter.WinPhone;
 
 namespace SimpleLifeCounter.WinPhone
 {
-    class SaveAndLoad_WinPhone : ISaveAndLoad
+    class SaveAndLoad_WinPhone
     {
-        // コピペ　動かん
-        public string LoadData(string filename)
-        {
-            var task = LoadDataAsync(filename);
-            task.Wait(); // HACK: to keep Interface return types simple (sorry!)
-            return task.Result;
-        }
-        async Task<string> LoadDataAsync(string filename)
-        {
-            var local = Windows.Storage.ApplicationData.Current.LocalFolder;
-            if (local != null)
-            {
-                var file = await local.GetItemAsync(filename);
-                using (StreamReader streamReader = new StreamReader(file.Path))
-                {
-                    var text = streamReader.ReadToEnd();
-                    return text;
-                }
-            }
-            return "";
-        }
-        public async void SaveData(string filename, string text)
-        {
-            var local = Windows.Storage.ApplicationData.Current.LocalFolder;
-            var file = await local.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
-            using (StreamWriter writer = new StreamWriter(await file.OpenStreamForWriteAsync()))
-            {
-                writer.Write(text);
-            }
-        }
-
-        public bool ClearData(string filename)
-        {
-            var task = ClearDataAsync(filename);
-            task.Wait(); // HACK: to keep Interface return types simple (sorry!)
-            return task.Result;
-        }
-
-        public async Task<bool> ClearDataAsync(string filename)
-        {
-            var local = Windows.Storage.ApplicationData.Current.LocalFolder;
-            await local.DeleteAsync();
-
-            try
-            {
-                var file = await local.GetFileAsync(filename);
-            }
-            catch (FileNotFoundException)
-            {
-                return true;
-            }
-            return false;
-        }
+        
     }
 }
