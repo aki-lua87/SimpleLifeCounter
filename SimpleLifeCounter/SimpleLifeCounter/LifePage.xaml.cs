20f2dd79bfb6a33fc3640cb0972dc95dffc13b2b
aki_lua87 <aki_lua87@outlook.jp>
2016-06-28 13:43:39 +0900

    LifePageに戻った際にライフをデフォルト値に

--- SimpleLifeCounter/SimpleLifeCounter/LifePage.xaml.cs
+++ SimpleLifeCounter/SimpleLifeCounter/LifePage.xaml.cs
@@ -52,6 +52,9 @@ namespace SimpleLifeCounter
             base.OnAppearing();
             model.JsonRead();
             BindingContext = model.getVm();
+            // もしデフォルトライフとVMの値が違うならデフォルトライフに代入して適用とかにする
+            LeftPlyerLife.Text = model.getStringDefaultLife();
+            RightPlyerLife.Text = model.getStringDefaultLife();
         }
 
     }
