18b16af3b54c7cfb30fa39aa4f000770a099dcd3
aki_lua87 <aki_lua87@outlook.jp>
2016-07-05 02:52:29 +0900

    UWP追加(ビルド通らず)

---
+++ SimpleLifeCounter/SimpleLifeCounter.UWP/SaveAndLoad_UWP.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Xamarin.Forms;
+using SimpleLifeCounter.UWP;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+using System;
+using System.Text;
+using System.Linq;
+using SimpleLifeCounter.Models;
+using Newtonsoft.Json;
+
+[assembly: Dependency(typeof(SaveAndLoad_UWP))]
+
+namespace SimpleLifeCounter.UWP
+{
+
+    class SaveAndLoad_UWP : ISaveAndLoad
+    {
+        private AllPageModel Model = new AllPageModel();
+        public string LoadData(string filename)
+        {
+            var json = JsonConvert.SerializeObject(Model);
+            return json;
+        }
+
+        public void SaveData(string filename, string text)
+        {
+            
+        }
+
+        public bool ClearData(string filename)
+        {
+            return true;
+        }
+
+
+
+    }
+}
