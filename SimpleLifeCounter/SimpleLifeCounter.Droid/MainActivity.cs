cf0bf01a57e62f82b63da857dfecacff150904df
aki_lua87 <aki_lua87@outlook.jp>
2016-08-16 21:25:00 +0900

    ランチャーの名前と起動時のアプリ名修正

--- SimpleLifeCounter/SimpleLifeCounter.Droid/MainActivity.cs
+++ SimpleLifeCounter/SimpleLifeCounter.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace SimpleLifeCounter.Droid
 {
-    [Activity(Label = "SimpleLifeCounter", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ライフカウンターくん for MTG", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
